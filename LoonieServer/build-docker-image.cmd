ac5d025713d2fb6ee732ebf45461ddf2a42a69ac
Niklas Dahlman <nd@nida.se>
2016-11-15 10:42:00 +0100

    sln

--- LoonieServer/build-docker-image.cmd
+++ LoonieServer/build-docker-image.cmd
@@ -1,1 +1,11 @@
 @echo off
+
+.paket\paket.exe restore
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
+
+docker build -t LoonieServer .
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
--- LoonieServer/build-docker-image.sh
+++ LoonieServer/build-docker-image.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+if test "$OS" = "Windows_NT"
+then
+else
+  mono .paket/paket.exe restore
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+  	exit $exit_code
+  fi
+
+  docker build -t LoonieServer .
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+    exit $exit_code
+  fi
+fi
