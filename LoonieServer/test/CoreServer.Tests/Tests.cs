40ae75ee2bb8c11f484036e687b80c791df35905
Niklas Dahlman <nd@sql8r.net>
2016-11-20 20:33:48 +0100

    .net core project and tests

---
+++ LoonieServer/test/CoreServer.Tests/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ConsoleApplication;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            LogEntry le = new LogEntry();
+            Assert.True(le!=null);
+        }
+    }
+}
