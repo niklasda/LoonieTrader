4b55ff85a3d72bafbeeeeb4b99b900bd94723163
Niklas Dahlman <nd@sql8r.net>
2017-01-04 20:23:38 +0100

    nuget update, red negative value converter

---
+++ LoonieTrader.App/ViewModels/Converters/IsNegativeConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Xceed.Wpf.DataGrid;
+
+namespace LoonieTrader.App.ViewModels.Converters
+{
+    public class IsNegativeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var dataCell = value as DataCell;
+            if (dataCell!= null)
+            {
+                if (dataCell.Content is decimal)
+                {
+                    return (decimal)dataCell.Content < 0m;
+                }
+
+                decimal dValue;
+                if (decimal.TryParse(dataCell.Content.ToString(), out dValue))
+                {
+                    return dValue < 0.0m;
+                    // return (double)value < 0.5d;
+                }
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
