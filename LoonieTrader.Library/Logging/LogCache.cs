e95d283a0016f38ae97524b2127f4eac4f8a4cb8
niklas <nd@nida.se>
2023-09-16 00:20:49 +0200

    remove some files

--- LoonieTrader.Library/Logging/LogCache.cs
+++
@@ -1,8 +0,0 @@
-﻿//using System.Collections.ObjectModel;
-
-//namespace LoonieTrader.Library.Logging;
-
-//public static class LogCache
-//{
-//    public static ObservableCollection<LogEntry> LogEntries { get; set; } = new ObservableCollection<LogEntry>();
-//}
